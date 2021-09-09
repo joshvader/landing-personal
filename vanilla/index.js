f9d19f0205665c882c796bb600e4c80cd38ae97e
proyect_icaro <josuevalenzuela87@gmail.com>
2021-09-09 10:01:07 -0300

    Arhivos Modificados

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,2 +1,8 @@
 let date = document.getElementById('date');
 date.innerText = `Fecha de hoy: ${new Date().toLocaleDateString()}`;
+
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
